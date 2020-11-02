5d11bdc480464c46c0ee7b335f3a146d57e43e07
Sourav Routh <67389838+SouravR7@users.noreply.github.com>
2020-11-02 11:42:34 +0530

    Changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,28 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState } from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [hide, sethide] = useState(false);
+  const handleClick = () => {
+    const newHide = !hide;
+    sethide(newHide);
+  };
+
   return (
     <div id="main">
-      // Do not alter the main div
+      <button id="click" onClick={handleClick}>
+        button
+      </button>
+      {hide === true ? (
+        <p id="para">
+          Hello, I've learnt to use the full-stack evaluation tool. This makes
+          me so happy
+        </p>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
-
 
 export default App;
